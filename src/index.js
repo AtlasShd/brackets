5030c685a767d65e2048f58f0516336158af1ead
Vlad <89317498+AtlasShd@users.noreply.github.com>
2022-01-26 22:02:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, conf) { // is it so easy?
+	while (conf.some(item => str.includes(`${item[0]}${item[1]}`))) {
+		conf.forEach(item => {
+			str = str.replace(`${item[0]}${item[1]}`, '');
+		});
+	}
+	return str ? false : true;
+};
